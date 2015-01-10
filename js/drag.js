45540659f077f44457a7295d341c5fa7211b2773
ccarruitero <cesar@mozilla.pe>
2015-01-10 18:51:40 -0500

    fix weird redirection for dataTransfer and handle drop event

--- js/drag.js
+++ js/drag.js
@@ -2,17 +2,16 @@ function handleDragStart(e) {
   e.target.style.opacity = '0.7';
   e.dataTransfer.effectAllowed = 'move';
   e.dataTransfer.dropEffect = 'move';
-  e.dataTransfer.setData('text/plain', 'nanana');
+  e.dataTransfer.setData('whatevertext', 'nanana');
 }
 
 function handleDragEnd(e) {
-  console.log('drag end');
+  e.target.style.opacity = '1';
 }
 
 function handleDragOver(e) {
   e.stopPropagation();
   e.preventDefault();
-
   e.dataTransfer.dropEffect = 'move';
 }
 
@@ -28,18 +27,23 @@ function handleDragLeave(e) {
   this.classList.remove('over');
 }
 
+function handleDrop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  this.classList.remove('over');
+}
+
 var tasks = document.querySelectorAll('.task');
 var taskBoxs = document.querySelectorAll('.task-box');
 
 [].forEach.call(tasks, function(task) {
-  console.log('fire this shit');
   task.addEventListener('dragstart', handleDragStart, false);
   task.addEventListener('dragend', handleDragEnd, false);
-  console.log('events setted');
 });
 
 [].forEach.call(taskBoxs, function(taskBox) {
   taskBox.addEventListener('dragenter', handleDragEnter, false);
   taskBox.addEventListener('dragover', handleDragOver, false);
   taskBox.addEventListener('dragleave', handleDragLeave, false);
+  taskBox.addEventListener('drop', handleDrop, false);
 });
